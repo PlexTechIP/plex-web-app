cee6c8f77c834558e0e7f773ece6a21eb4bde042
Eugene Chao <epchao@berkeley.edu>
2025-01-19 21:07:50 -0800

    Remove check for image existence and use new endpoint

--- src/app/_libs/fetchMembers.ts
+++ src/app/_libs/fetchMembers.ts
@@ -11,11 +11,12 @@ interface ApiResponse {
     instagram_username: string | null;
     calendly_username: string | null;
     current_company: string | null;
+    img_url: string | null;
   }[];
 }
 export const fetchMembers = async (): Promise<Member[]> => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/members/public-info`
+    `${process.env.NEXT_PUBLIC_URL}/public-members-info`
   );
   if (!response.ok) {
     throw new Error("Failed to fetch members");
@@ -23,33 +24,19 @@ export const fetchMembers = async (): Promise<Member[]> => {
 
   const data: ApiResponse = await response.json();
 
-  const checkImageExists = async (url: string): Promise<boolean> => {
-    try {
-      const res = await fetch(url, { method: "HEAD" });
-      return res.ok;
-    } catch {
-      return false;
-    }
-  };
-
-  const members = await Promise.all(
-    data.users.map(async (user) => {
-      const imageUrl = `${process.env.NEXT_PUBLIC_URL}/public/profile-pictures/${user.id}.jpg`;
-      const imageExists = await checkImageExists(imageUrl);
-
-      return {
-        firstName: user.first_name,
-        lastName: user.last_name,
-        imageUrl: imageExists ? imageUrl : undefined, // Only set if the image exists
-        position: user.current_position || "Unknown",
-        blurb: user.profile_blurb || "No blurb available",
-        linkedin: user.linkedin_username || undefined,
-        instagram: user.instagram_username || undefined,
-        calendly: user.calendly_username || undefined,
-        currentCompany: user.current_company || undefined,
-      };
-    })
-  );
+  const members = data.users.map((user) => {
+    return {
+      firstName: user.first_name,
+      lastName: user.last_name,
+      imageUrl: user.img_url || undefined,
+      position: user.current_position || "Unknown",
+      blurb: user.profile_blurb || "No blurb available",
+      linkedin: user.linkedin_username || undefined,
+      instagram: user.instagram_username || undefined,
+      calendly: user.calendly_username || undefined,
+      currentCompany: user.current_company || undefined,
+    };
+  });
 
   return members;
 };
