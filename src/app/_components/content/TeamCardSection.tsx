a9b91050dfcc82c61ab40f60737fad7303349a23
Merrick Zheng <66280529+EggsInAJar@users.noreply.github.com>
2026-01-19 20:50:59 -0800

    center team cards

--- src/app/_components/content/TeamCardSection.tsx
+++ src/app/_components/content/TeamCardSection.tsx
@@ -12,7 +12,7 @@ interface TeamCardSectionProps {
 
 const TeamCardSection: React.FC<TeamCardSectionProps> = ({ members }) => {
   return (
-    <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6`}>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
       {members.map((member, index) => {
         return (
           <div key={index} className="flip-card w-[85%]">
